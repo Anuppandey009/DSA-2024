02ff72c6a5b9aad1918199b06f3f2058a8cb19c8
Anup pandey <anuppandey000@gmail.com>
2024-06-02 10:02:31 +0530

    sort the array using recursion

---
+++ Recursion/problem_1.js
@@ -0,0 +1,35 @@
+
+// Sort th aray using recursion
+
+let arr =[2,3,7,6,4,5,9]
+
+
+function sort (arr){
+  if(arr.length<=1){
+    return
+  }
+  let temp =arr[arr.length-1]
+    arr.pop()
+    sort(arr)
+    insert(arr,temp)
+  
+}
+
+
+
+function insert(arr,temp){
+  if(arr.length==0 || arr[arr.length-1]<=temp){
+    arr.push(temp)
+    return
+  }
+  
+  let value= arr[arr.length-1]
+  arr.pop()
+  insert(arr,temp)
+  arr.push(value)
+}
+
+
+sort(arr)
+
+console.log(arr);
