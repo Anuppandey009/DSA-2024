15a27f33033fdb8b1c4d4427c907a4d227cb416c
Anup pandey <anuppandey000@gmail.com>
2024-06-09 11:35:57 +0530

    Sum of digit of a number using recursion

---
+++ Recursion/problem_10.js
@@ -0,0 +1,17 @@
+
+
+//Sum of digit of a number using recursion
+
+let n = 125
+let sum
+
+function digitSum(n) {
+    if (n == 0) {
+        return 0
+    }
+    sum = n % 10 + digitSum(Math.floor(n / 10))
+    return sum
+}
+
+digitSum(n)
+console.log(sum)
