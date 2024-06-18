ba300b4eb4e27353567e20a5838d8e97a3673af3
Akash Chandra <chandra.akash020@gmail.com>
2024-06-18 23:36:19 +0530

    Bubble Sort modified

--- Sortings/bubbleSort.js
+++ Sortings/bubbleSort.js
@@ -1,3 +1,15 @@
+/*  1. Bubble Sort  */
+/*
+Description: Compares adjacent elements and swaps them if they are in the wrong order.This process is repeated until the list is sorted.
+
+Time Complexity: O(n ^ 2)
+
+Space Complexity: O(1)
+
+Use Case: Simple but inefficient for large datasets.Often used for educational purposes.
+*/
+
+
 let arr = [8, 2, 3, 7, 1, 6, 4, 5, 9]
 console.log('Initial arr:', arr)
 
